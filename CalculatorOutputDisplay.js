5b9890f8bba4cf98c4c101bc702cf54446842d9d
Punchh_Kajal <punchh_kajal@Punchh-Kajals-MacBook-Pro.local>
2018-10-13 18:20:38 +0530

    -Minor changes into UI.

--- CalculatorOutputDisplay.js
+++ CalculatorOutputDisplay.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Platform, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
+const { height } = Dimensions.get('window');
 
 class CalculatorOutputDisplay extends Component {
 	render() {
 		return (
-			<View
-				style={{
-					backgroundColor: '#333333',
-					height: 200,
-					width: '100%',
-					alignItems: 'center',
-					justifyContent: 'center'
-				}}
-			>
-				<Text style={{ color: 'white', fontSize: 50, fontWeight: 'bold', alignItems: 'center' }}>
-					{this.props.value}
-				</Text>
-			</View>
+			<ScrollView centerContent={true}>
+				<Text style={styles.textStyle}>{this.props.value}</Text>
+			</ScrollView>
 		);
 	}
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	textStyle: {
+		color: 'white',
+		fontSize: 50,
+		fontWeight: 'bold',
+		...Platform.select({
+			ios: {
+				padding: height === 812 ? 30 : 20
+			},
+			android: {
+				padding: 20
+			}
+		})
+	}
+});
 // Pass it as the first argument to our connect function.
 const mapStateToProps = state => {
 	return {
